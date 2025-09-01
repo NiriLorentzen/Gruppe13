5aba654b44e15f1052f9144b0ce0a4481fc79aad
William Jondahl <142999366+Williamjondahl@users.noreply.github.com>
2025-09-01 14:45:22 +0200

    La til beskrivelse div på index (#5)

--- index.php
+++ index.php
@@ -14,9 +14,18 @@ include "navbar.php"
     <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
-     <h1>Gruppe 21s nettside</h1>
-
-     
+    <h1>Gruppe 21s nettside</h1>
+        <div class="description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec nisi sit amet arcu facilisis pretium ut non nulla. 
+            Ut neque ex, tempor vel bibendum laoreet, tincidunt non arcu. Nullam luctus ac lorem sit amet pharetra. 
+            Nunc erat neque, pellentesque vitae ligula et, suscipit efficitur nisl. Morbi volutpat suscipit lacus, porttitor laoreet purus ornare id. 
+            Sed laoreet, diam et bibendum facilisis, lacus metus ultrices sapien, nec ultrices risus quam non quam. Aenean convallis nisl nec imperdiet pretium. Quisque volutpat lacus id nisi elementum, id mattis orci ultrices. 
+            Phasellus sem augue, cursus vitae nunc eget, faucibus rhoncus tortor. Vivamus condimentum velit sed metus volutpat tincidunt. 
+            Sed tempor mauris augue, quis ultrices sem pulvinar pretium. Sed nec est mollis, fringilla mauris at, mattis nulla. Fusce orci augue, fringilla in justo ut, condimentum condimentum augue. 
+            Ut vel auctor sapien. Nunc at lacus et ante eleifend dictum. Maecenas varius in magna et viverra. Nulla pharetra, ante eu accumsan dictum, orci orci feugiat nulla, nec sagittis purus purus non tellus. 
+            Praesent pretium, arcu ut mattis maximus, augue ex bibendum mi, eu mollis est purus non dolor. Ut interdum enim id condimentum sollicitudin. Aenean imperdiet arcu ut mi pellentesque sodales. Vivamus quis tellus condimentum, facilisis metus ac, porttitor mauris.
+            </p>
+        </div>
 
 </body>
 </html>
