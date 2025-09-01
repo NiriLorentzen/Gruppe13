272025fc64963b1538f40900b89c131082b92ce2
Williamjondahl <williamj@uia.no>
2025-09-01 15:14:23 +0200

    Lagt til bilde på medlemmer page

--- medlemmer.php
+++ medlemmer.php
@@ -45,7 +45,7 @@ class Members {
 $members = [
     new Members("Eivind", "Frontend utvikler", "Går på UiA, IT og Informasjonssystemer", "images/file.svg"),
     new Members("Niri", "Frontend utvikler", "Går på UiA, IT og Informasjonssystemer", "images/file.svg"),
-    new Members("William", "Frontend utvikler", "Går på UiA, IT og Informasjonssystemer", "images/file.svg"),
+    new Members("William", "Frontend utvikler", "Går på UiA, IT og Informasjonssystemer", "images/williamj.jpg"),
     new Members("Carl", "Frontend utvikler", "Går på UiA, IT og Informasjonssystemer", "images/CarlBilde.jpg"),
     new Members("Nicolay", "Frontend utvikler", "Går på UiA, IT og Informasjonssystemer", "images/NicolayBilde.jpg")
 ];
