09394f2fff0dcc9d894f7173e025ae3c238fff52
CarlAndreasArnegard <carlaa@uia.no>
2025-09-02 13:14:39 +0200

    Added yellow color to active page on navbar

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="stylesheet.css">
 </head>
 <div class="navbar">
-    <img src="images/Logo.svg" width=75rem length=75rem>
-    <a href="index.php">Hovedside</a>
-    <a href="medlemmer.php">Medlemmer</a>
+    <img src="images/Logo.svg" width="75" length="75">
+    <a href="index.php" class="<?php if(basename($_SERVER['PHP_SELF']) == 'index.php') echo 'active'; ?>">Hovedside</a>
+    <a href="medlemmer.php" class="<?php if(basename($_SERVER['PHP_SELF']) == 'medlemmer.php') echo 'active'; ?>">Medlemmer</a>
 </div>
